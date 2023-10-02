ba61440d1e24b5a8942dae03d6cedf1a6cd2d39e
unknown <ethanieong99@gmail.com>
2023-10-02 02:58:07 -0400

    IDK ANYMORE

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import  {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -13,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false, headerLeft: ()=> null}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, headerLeft: () => null }} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false, headerLeft: () => null }} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity, StyleSheet, Image, ScrollView, Dimensions, View, Text, Pressable, Modal } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, TouchableOpacity, SafeAreaView, Pressable, ScrollView, Modal } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-
+import NetInfo from '@react-native-community/netinfo';
 
 
 
@@ -9,42 +9,64 @@ export default function Home({ navigation }) {
 
 
     return (
-        <View style={{ flex: 1 }}>
-            <ScrollView style={{ backgroundColor: '#f5f5f5', marginTop: '10%', height: '100%' }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center', 'height': '10%' }}>
-                    <Text adjustsFontSizeToFit style={{ fontSize: 30, fontWeight: 'bold', color: 'black', fontStyle: 'italic', left: '30%', top: '50%' }}>
-                        Welcome {'\n'}
-                        <Text adjustsFontSizeToFit style={{ fontSize: 15 }}>placeholder</Text>
-                    </Text>
-                    <View style={{ position: 'absolute', right: '20%', top: '15%', width: '30%', height: '20%', borderRadius: Dimensions.get('window').height / 30, resizeMode: 'contain' }}>
-                        <Ionicons name={"person-circle-outline"} size={Dimensions.get('window').height / 10} color="purple" style={{ alignSelf: 'center', top: '20%', }} />
-                    </View>
-                    <View style={styles.card}>
-                        <Pressable style={styles.circle} onPress={() => { console.log('hi') }}>
-                            <Image source={require('../assets/placeholderLogo.png')} style={{ resizeMode: 'contain', width: 100, height: 100, alignSelf: 'center', marginTop: Dimensions.get('window').height / 20 }} />
-                        </Pressable>
-                    </View>
-                    <View style = {{flexDirection:'row',justifyContent:'center',top:'3%',width:'90%',height:'40%',borderRadius:Dimensions.get('window').height/20,alignSelf: 'center'}}>
-                        <ContactWidget name = {"chatbox-outline"} size={Dimensions.get('window').height / 20} color="purple" style={{ alignSelf: 'center', marginTop: Dimensions.get('window').height / 40 }} onPress={()=>{console.log('Hello')}}/>
-                        <ContactWidget name = {"videocam-outline"} size={Dimensions.get('window').height / 20} color="purple" style={{ alignSelf: 'center', marginTop: Dimensions.get('window').height / 40 }} onPress={()=>{console.log('Hello')}}/>
-                        <ContactWidget name = {"call-outline"} size={Dimensions.get('window').height / 20} color="purple" style={{ alignSelf: 'center', marginTop: Dimensions.get('window').height / 40 }} onPress={()=>{console.log('Hello')}}/>
-                        <ContactWidget image={true} size={Dimensions.get('window').height / 20} color="purple" style={{ alignSelf: 'center', marginTop: Dimensions.get('window').height / 40 }} onPress={()=>{console.log('Hello')}}/>
-                    </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', fontStyle: 'italic', top: '7%', left: '20%' }}>
+                    Welcome
+                </Text>
+                <TouchableOpacity style={{ top: '6%', left: '500%' }} onPress={() => { profile}}>
+                    <Ionicons name="person-circle-outline" size={40} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={{ height: 1, backgroundColor: 'black', opacity: 0.5, top: '5%' }} />
+            <View style={styles.card}>
+                <View style={{ height: '100%' }}>
+                    <TouchableOpacity style={styles.circle}>
+                        <Image source={require("../assets/favicon.png")} style={{ alignSelf: 'center', top: '30%' }} />
+                    </TouchableOpacity>
+                    <ContactFriend object={"heart"} method={placeholder} bordercolor={"red"} index={4} />
+                    <ContactFriend object={"chatbox-outline"} method={placeholder} index={3} />
+                    <ContactFriend object={"videocam-outline"} method={placeholder} index={2} />
+                    <ContactFriend object={"call-outline"} method={placeholder} index={1} />
+
+                    <OnlineIndicator />
                 </View>
-            </ScrollView>
-        </View>
+                <Text adjustsFontSizeToFit style={{ fontSize: 30, left: '12%', opacity: 0.5, bottom: '30%' }}>
+                    Name
+                </Text>
+            </View>
+            <Text style={{ fontSize: 20, color: 'lightgrey', fontWeight: 'bold', alignSelf: 'center', top: '1%' }}>
+                Discover:
+            </Text>
+            <View style={{ flexDirection: 'row' }}>
+                <SmallWidget text={"Reminder:"} onPress={placeholder} iconName={"alarm-outline"} />
+                <SmallWidget text={"Calendar:"} onPress={placeholder} iconName={"calendar-outline"} />
+            </View>
+            <View style = {{top: '10%'}}>
+                <Text style={{ textAlign: 'center', top: '10%', color: 'lightgrey', }}>
+                    This was made by: {''}
+                </Text>
+                <Text style={{ textAlign: 'center', color: 'lightgrey', fontWeight: 'bold', fontStyle: 'italic', }}>
+                    Ethan Ieong
+                </Text>
+                <View style={{ flexDirection: 'row', left:'10%'}}>
+                    <Text style={{ textAlign: 'center', color: 'lightgrey', fontWeight: 'bold', fontStyle: 'italic',left:'300%' }}>Github: </Text>
+                    <Text style={{ textAlign: 'center', color: 'lightgrey', fontWeight: 'bold', fontStyle: 'italic',left:'300%' }}>https://github.com/etthann/ihateRN</Text>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     card: {
-        width: Dimensions.get('window').width / 1.1,
-        height: Dimensions.get('window').height / 2.2,
+        width: '80%',
+        height: '30%',
         backgroundColor: 'lightblue',
-        borderRadius: Dimensions.get('window').height / 20,
+        borderRadius: 10,
         alignSelf: 'center',
-        marginBottom: Dimensions.get('window').height / 20,
-        marginTop: Dimensions.get('window').height / 20,
+        marginBottom: '5%',
+        marginTop: '20%',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -53,18 +75,16 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.22,
         shadowRadius: 2.22,
         elevation: 3,
-    },circle: {
+    },
+    circle: {
         backgroundColor: 'white',
-        width: Dimensions.get('window').width / 2,
-        height: Dimensions.get('window').width / 2,
+        width: '40%',
+        height: '60%',
         borderColor: '#e8e8e8',
         borderWidth: 1,
-        borderRadius: Dimensions.get('window').height / 4,
-        marginTop: Dimensions.get('window').height / 50,
-        marginLeft: Dimensions.get('window').width / 20,
-        alignSelf: 'center',
-        right: Dimensions.get('window').width / 35,
-        padding: Dimensions.get('window').height / 100,
+        borderRadius: 100,
+        top: '3%',
+        left: '3%',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -73,28 +93,94 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.22,
         shadowRadius: 2.22,
         elevation: 3,
-    },contactWidget: {
+    },
+    smallWidget: {
         backgroundColor: 'pink',
-        width: Dimensions.get('window').width / 6,
-        height: Dimensions.get('window').width / 6,
-        borderRadius: Dimensions.get('window').height / 12,
-        alignSelf: 'center',
-        marginLeft: Dimensions.get('window').width / 20,
+        width: '35%',
+        height: '110%',
+        borderRadius: 10,
+        left: '32%',
+        top: '5%',
         shadowColor: "#000",
-        justifyContent: 'space-between',
         shadowOffset: {
             width: 0,
             height: 1,
         },
-    
-}})
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+        marginHorizontal: '4%',
+    },
+})
 
-function ContactWidget({name,onPress,image})  {
-    return(
-        <TouchableOpacity style = {styles.contactWidget} onPress={onPress}>
-            {!image ? (
-                <Ionicons name = {name} size = {Dimensions.get('window').height/20} color = "purple" style = {{alignSelf:'center',top:Dimensions.get('window').height/40}} />): 
-                <Image source={require('../assets/splash.png')} color="purple" style={{ resizeMode: 'contain', width: Dimensions.get('window').height / 11, bottom: Dimensions.get('window').height / 30, right: Dimensions.get('window').height / 200, }} />}
+const placeholder = () => {
+    console.log("hi")
+}
+
+const OnlineIndicator = () => {
+    const [isOnline, setIsOnline] = useState(false);
+
+    useEffect(() => {
+        const checkOnlineStatus = async () => {
+            const netInfo = await NetInfo.fetch();
+            setIsOnline(netInfo.isConnected);
+        };
+
+        checkOnlineStatus();
+
+        const unsubscribe = NetInfo.addEventListener(state => {
+            setIsOnline(state.isConnected);
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    // Determine the dot color based on online status
+    const dotColor = isOnline ? 'green' : 'grey';
+
+    return <View style={[styles.dot, { backgroundColor: dotColor, width: '10%', height: '15.6%', borderRadius: 200, left: '30%', bottom: '105%', borderWidth: 1, top: '-11%' }]} />;
+};
+
+function ContactFriend({ method, object, bordercolor, index }) {
+    const positionStyle = {
+        position: 'absolute',
+        bottom: `${index * 20 - 10}%`, // Adjust this value to space the icons vertically
+        left: '80%',
+    };
+
+    return (
+        <TouchableOpacity onPress={method} style={positionStyle}>
+            <Ionicons name={object} size={40} color={bordercolor} />
         </TouchableOpacity>
     );
+}
+
+
+function SmallWidget({ text, onPress, iconName }) {
+    return (
+        <TouchableOpacity
+            style={styles.smallWidget}
+            onPress={onPress}
+        >
+            <Text style={{ fontSize: 20, fontWeight: 'bold', padding: '10%' }}>
+                {text}
+            </Text>
+            <Ionicons name={iconName} size={40} color="purple" style={{ alignSelf: 'center' }} />
+        </TouchableOpacity>
+    );
+}
+
+
+const profile = () => {
+    const [showProfile, setShowProfile] = useState(false);
+
+    return (
+        <Modal animationType='fade-in' transparent={true} visible={showProfile} onRequestClose={() => setShowProfile(false)} style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'white'}}>
+            <View style = {{height: '100%', width: '100%', backgroundColor: 'black'}}>
+
+            </View>
+        </Modal>
+    )
 }
